d7c617faeada55695bff99c8e9610be8b18167be
Arjun Dass <ad.sam007@gmail.com>
2024-10-28 11:28:08 -0700

    backend: add renew loan endpoint

--- backend/src/main/java/com/fullstack/bookstore/controller/BookController.java
+++ backend/src/main/java/com/fullstack/bookstore/controller/BookController.java
@@ -50,4 +50,10 @@ public class BookController {
         String userEmail = JWTExtraction.payloadJWTExtraction(token, "\"sub\"");
         bookService.returnBook(userEmail, bookId);
     }
+
+    @PutMapping("/secure/renew/loan")
+    public void renewLoan(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = JWTExtraction.payloadJWTExtraction(token, "\"sub\"");
+        bookService.renewLoan(userEmail, bookId);
+    }
 }
