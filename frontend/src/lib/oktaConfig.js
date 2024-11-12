8329e26d80dafc02e239f757e6169e8f98248ce0
Arjun Dass <ad.sam007@gmail.com>
2024-11-11 17:20:41 -0800

    backend: add cert to backend end points

--- frontend/src/lib/oktaConfig.js
+++ frontend/src/lib/oktaConfig.js
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oajzen8gySwFjozG5d7',
     issuer: 'https://dev-53658902.okta.com/oauth2/default',  // Added /oauth2/default
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true, // Not recommended for production,
